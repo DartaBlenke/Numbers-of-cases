dd2fac9a9d8cb85770435a4c7e2ee81a254cd1f9
D'Artagnan Blenke <dartagnan.blenke17@gmail.com>
2021-11-17 23:32:53 -0300

    days counter

--- src/dtos/CasesDTO.ts
+++
@@ -1,4 +0,0 @@
-export interface CasesDto {
-    day: number[];
-    cases_data: number[];
-}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { ScrollView } from 'react-native';
 
 import { Button } from '../../components/Button';
-import { api } from '../../services/api';
-import { CasesDto } from '../../dtos/CasesDTO';
 
 import { 
   Screen,
@@ -15,56 +14,68 @@ import {
   Cases_number,
 } from './styles';
 
+interface DateProps
+{
+  day: number;
+  cases: number;
+}
+
 export function Home(){
   
-  const [day, setDay] = useState(0);
-  const [dataCases, setDataCases] = useState<CasesDto[]>([]);
-  const [loadint, setLoading] = useState(true);
+  const [day, setDay] = useState(1);
+  const [dataCases, setDataCases] = useState<DateProps[]>([{day: day, cases: 634}]);
 
   function OneLessDay(){
-    if (day == 0) {
-      return;
-    } setDay(day -1)
+    if (dataCases.length > 0) {
+      var data = dataCases;
+      data.pop();
+      setDataCases(data);
+    }
+  }
+  
+  function randomNumber() {
+    Math.floor(Math.random() * 1 - 1000);
   }
 
-  useEffect(() => {
-    async function cases() {
-      try {
-        const response = await api.get('/api/v1/cases');
-        setDataCases(response.data);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    }
-    cases();
-  },[]);
+  function AddDay(){
+    var day = dataCases.length + 1;
+    var cases = 777;
+    var data = dataCases;
+    data.push({day, cases});
+    setDataCases(data);
+    console.log(dataCases);
+  }
 
   return(
     <Screen>
       <Title>Quantidade de casos de COVID-19 em dias</Title>
       <Body>
         <Days_scores>
-          <Cases_number>
-            {dataCases}
-          </Cases_number>
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+          >
+            {dataCases.map(dataCase => ( 
+              <Cases_number>
+                {dataCase.day} - {dataCase.cases}
+              </Cases_number>
+            ))}
+          </ScrollView>
         </Days_scores>
 
         <Quantity_days>
-          <Days>{day}</Days>
+         <Days>{dataCases[dataCases.length - 1].day}</Days>
         </Quantity_days>
       </Body>
       <Footer>
         <Button 
           text="- 1 dia"
           way="down"
-          onPress={OneLessDay}
+          onPress={() => OneLessDay()} 
         />
         <Button 
           text="+ 1 dia"
           way="up"
-          onPress={() => setDay( day + 1 )}
+          onPress={() => AddDay()}
         />
       </Footer>
     </Screen>
